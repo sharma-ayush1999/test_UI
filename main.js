bd9b4f52b26a2c2a1e0a2439db7ecf084c28f77b
Ayush Sharma <sharma.ayush3103@gmail.com>
2020-04-28 17:36:57 +0530

    changes

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 document.getElementById('clear').addEventListener('click',function(){
-    if(window.screen.width < 501){
+        if(document.getElementById('clear-left').style.display=='none'){
+            document.getElementById('clear-left').style.display='block';
+        }
+        else{
+            document.getElementById('clear-left').style.display='none';
+        }
+});
+document.getElementById('btn-hide').addEventListener('click',function(){
     if(document.getElementById('clear-left').style.display=='none'){
         document.getElementById('clear-left').style.display='block';
     }
     else{
         document.getElementById('clear-left').style.display='none';
     }
-}
-    
-})
+});
+
+document.getElementById('main-icon').addEventListener('click',function(){
+    document.getElementById('overlay').style.display="block";
+    document.getElementById('body-overlay').style.display="block";
+});
+document.getElementById('overlay-btn1').addEventListener('click',function(){
+    document.getElementById('overlay').style.display="none";
+    document.getElementById('body-overlay').style.display="none";
+});
+document.getElementById('overlay-btn2').addEventListener('click',function(){
+    document.getElementById('overlay').style.display="none";
+    document.getElementById('body-overlay').style.display="none";
+});
+
+
